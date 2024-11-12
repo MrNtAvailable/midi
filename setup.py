b94f21fde5d9203807274086db1805b5137a958b
MrNtAvailable <97128886+MrNtAvailable@users.noreply.github.com>
2024-11-11 18:29:17 -0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='rbmidi',
+    packages=find_packages(include=['rbmidi']),
+    version='0.1.0',
+    description='Robby Bobbys Midi Library',
+    author='Robert Masotti',
+)
